411819a9d38bbe45ee934194c063f013ceab1bce
Rajat Naik <36617903+rajatnaik12@users.noreply.github.com>
2020-06-08 16:32:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -13,6 +13,18 @@ class App extends Component {
         this.handleChange = this.handleChange.bind(this)
     }
 
+    /** The handle changed method is actually used to handle the
+    * changes on checkboxes but as if anyone try that tutorial exactly in 2020 then result will be not 
+    * as excepted and if you have tried then the handleChange is being called two times. Thats why I have created 
+    * this repository to keep those learning react motivated.
+    * If you see below and the content on the video then you will notice that I have created toUpdateTodos
+    * and sliced the state of todos. Because you should not change the state directly and as in the video is was
+    * generating the bug.
+    * check this official documentation and tutorial at https://reactjs.org/tutorial/tutorial.html#why-immutability-is-important
+    * I insist to read and try the tutorial. 
+    * # Using this approach it becomes easy to detect changes
+    * # Also it helps in Determining When to Re-Render in React.
+    */
     handleChange(id) {
         const toUpdateTodos = this.state.todos.slice()
         const updatedTodos = toUpdateTodos.map(todo => {
